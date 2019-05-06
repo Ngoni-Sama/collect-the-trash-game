236ffc598862b92997a433368f08a0b9e66cc63a
Shivam Bansal <cs1170376@iitd.ac.in>
2019-05-06 17:34:00 +0530

    some values changed
    Co-Authored-By: vijay2411 <vijay2411@users.noreply.github.com>

---
+++ Unity_game/Assets/scripts/GameOverManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class GameOverManager : MonoBehaviour
+{
+    public GameObject player;       // Reference to the player's health.
+    public float restartDelay = 5f;         // Time to wait before restarting the level
+
+
+    Animator anim;                          // Reference to the animator component.
+    float restartTimer;                     // Timer to count up to restarting the level
+
+
+    void Awake()
+    {
+        // Set up the reference.
+
+        player = GameObject.FindGameObjectWithTag("player");
+    }
+
+
+    void Update()
+    {
+        // If the player has run out of health...
+        if (player.GetComponent<player_controller>().pollution>=100 || player.GetComponent<player_controller>().timeLeft <= 0)
+        {
+            // ... tell the animator the game is over.
+            anim.SetTrigger("GameOver");
+
+            // .. increment a timer to count up to restarting.
+            restartTimer += Time.deltaTime;
+
+            // .. if it reaches the restart delay...
+            if (restartTimer >= restartDelay)
+            {
+                // .. then reload the currently loaded level.
+               // SceneManager.LoadScene(Application.loadedLevel);
+            }
+        }
+    }
+}
