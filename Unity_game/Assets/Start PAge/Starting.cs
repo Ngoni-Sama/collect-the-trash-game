fbe3973ee84901c7715000e60eba60580a9385de
Shivam Bansal <cs1170376@iitd.ac.in>
2019-05-06 21:33:59 +0530

    randomly temporary

---
+++ Unity_game/Assets/Start PAge/Starting.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Starting : MonoBehaviour {
+
+    // Use this for initialization
+    [SerializeField]
+    private string scene="game scene" ;
+    public void StartGame() {
+        SceneManager.LoadScene(scene);
+    }
+}
