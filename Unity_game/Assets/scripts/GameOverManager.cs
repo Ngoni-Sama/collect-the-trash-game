d6d3c53eac624751c6c6237680377108f4987c1e
Shivam Bansal <cs1170376@iitd.ac.in>
2019-05-06 19:05:38 +0530

    score added
    Co-Authored-By: vijay2411 <vijay2411@users.noreply.github.com>

--- Unity_game/Assets/scripts/GameOverManager.cs
+++ Unity_game/Assets/scripts/GameOverManager.cs
@@ -1,11 +1,11 @@
 ﻿using UnityEngine;
-
+using UnityEngine.UI;
 public class GameOverManager : MonoBehaviour
 {
     public GameObject player;       // Reference to the player's health.
     public float restartDelay = 5f;         // Time to wait before restarting the level
-
-
+    public GameObject scoreBoard;
+    private bool GameOver=false;
     Animator anim;                          // Reference to the animator component.
     float restartTimer;                     // Timer to count up to restarting the level
 
@@ -15,17 +15,19 @@ public class GameOverManager : MonoBehaviour
         // Set up the reference.
         anim = GetComponent<Animator>();
         player = GameObject.FindGameObjectWithTag("Player");
+        scoreBoard = GameObject.FindGameObjectWithTag("score");
     }
 
 
     void Update()
     {
         // If the player has run out of health...
-        if (player.GetComponent<player_controller>().pollution <= 0 || player.GetComponent<player_controller>().pollution>=100 || player.GetComponent<player_controller>().timeLeft <= 0)
+        if ((player.GetComponent<player_controller>().pollution <= 0 || player.GetComponent<player_controller>().pollution>=100 || player.GetComponent<player_controller>().timeLeft <= 0) && !GameOver)
         {
+            scoreBoard.GetComponent<Text>().text="Score: " + (int)((10000000)/((player.GetComponent<player_controller>().pollution+1)*(player.GetComponent<player_controller>().timeLeft+1)));
             // ... tell the animator the game is over.
             anim.SetTrigger("gameover");
-
+            GameOver = true;
             // .. increment a timer to count up to restarting.
             restartTimer += Time.deltaTime;
 
